03c1ed22b8246ed73afad8edef901a106fa0ac9a
SaiKrishna7211 <21d41a7211@gmail.com>
2025-09-09 17:51:27 +0530

    js start

---
+++ JS/Day_01/script.js
@@ -0,0 +1,11 @@
+// let a=+prompt("Enter a value : "),b=+prompt("Enter b value : ");
+// console.log("the addition is : "+(a+b))
+// console.log("the subtraction is : "+(a-b))
+// console.log("the multiplication is : "+(a*b))
+// console.log("the division is : "+(a/b))
+
+// area of rectanhle
+
+let length = +prompt("Enter rectangle length : ");
+let breadth = +prompt("Enter reactangle breadth : ");
+console.log("The area of your Rectangle is : "+(length*breadth));
